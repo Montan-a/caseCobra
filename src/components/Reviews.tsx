aea94955987d42b49341f22fbcaa9716d3af576d
Montana Shaw <88953023+Montan-a@users.noreply.github.com>
2024-07-12 16:13:49 -0500

    fixed marquee animation

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,8 +1,9 @@
 "use client";
-import React, { HTMLAttributes, useEffect, useRef, useState } from "react";
+
+import { HTMLAttributes, useEffect, useRef, useState } from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { useInView } from "framer-motion";
-import { cn, splitArray } from "@/lib/utils";
+import { cn } from "@/lib/utils";
 import Phone from "./Phone";
 
 const PHONES = [
@@ -13,6 +14,20 @@ const PHONES = [
   "/testimonials/5.jpg",
   "/testimonials/6.jpg",
 ];
+
+function splitArray<T>(array: Array<T>, numParts: number) {
+  const result: Array<Array<T>> = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const index = i % numParts;
+    if (!result[index]) {
+      result[index] = [];
+    }
+    result[index].push(array[i]);
+  }
+
+  return result;
+}
 
 function ReviewColumn({
   reviews,
@@ -26,22 +41,23 @@ function ReviewColumn({
   msPerPixel?: number;
 }) {
   const columnRef = useRef<HTMLDivElement | null>(null);
-
-  const [columnHeight, setColumnHieght] = useState(0);
-
+  const [columnHeight, setColumnHeight] = useState(0);
   const duration = `${columnHeight * msPerPixel}ms`;
 
   useEffect(() => {
     if (!columnRef.current) return;
 
     const resizeObserver = new window.ResizeObserver(() => {
-      setColumnHieght(columnRef.current?.offsetHeight ?? 0);
+      setColumnHeight(columnRef.current?.offsetHeight ?? 0);
     });
 
     resizeObserver.observe(columnRef.current);
 
-    return () => resizeObserver.disconnect();
+    return () => {
+      resizeObserver.disconnect();
+    };
   }, []);
+
   return (
     <div
       ref={columnRef}
@@ -64,7 +80,7 @@ interface ReviewProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 function Review({ imgSrc, className, ...props }: ReviewProps) {
-  const POSSIBLE_ANIMATION_DELAY = [
+  const POSSIBLE_ANIMATION_DELAYS = [
     "0s",
     "0.1s",
     "0.2s",
@@ -74,14 +90,14 @@ function Review({ imgSrc, className, ...props }: ReviewProps) {
   ];
 
   const animationDelay =
-    POSSIBLE_ANIMATION_DELAY[
-      Math.floor(Math.random() * POSSIBLE_ANIMATION_DELAY.length)
+    POSSIBLE_ANIMATION_DELAYS[
+      Math.floor(Math.random() * POSSIBLE_ANIMATION_DELAYS.length)
     ];
 
   return (
     <div
       className={cn(
-        "animate-fade-in rounded-2.25rem bg-white p-6 opacity-0 shadow-xl shadow-slate-900/5",
+        "animate-fade-in rounded-[2.25rem] bg-white p-6 opacity-0 shadow-xl shadow-slate-900/5",
         className
       )}
       style={{ animationDelay }}
@@ -99,10 +115,11 @@ function ReviewGrid() {
   const column1 = columns[0];
   const column2 = columns[1];
   const column3 = splitArray(columns[2], 2);
+
   return (
     <div
       ref={containerRef}
-      className="relative -mx-4 mt-16 grid-h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2"
+      className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3"
     >
       {isInView ? (
         <>
@@ -131,22 +148,24 @@ function ReviewGrid() {
           />
         </>
       ) : null}
+      <div className="pointer-events-none absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-slate-100" />
+      <div className="pointer-events-none absolute inset-x-0 bottom-0 h-32 bg-gradient-to-t from-slate-100" />
     </div>
   );
 }
 
-const Reviews = () => {
+function Reviews() {
   return (
     <MaxWidthWrapper className="relative max-w-5xl">
       <img
-        src="/what-people-are-buying.png"
-        alt="What people are buying"
         aria-hidden="true"
+        src="/what-people-are-buying.png"
         className="absolute select-none hidden xl:block -left-32 top-1/3"
       />
+
       <ReviewGrid />
     </MaxWidthWrapper>
   );
-};
+}
 
 export default Reviews;
