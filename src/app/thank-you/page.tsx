d1cdb250dadff8965cf503cd7fc1eba7b7ed97ed
Montana Shaw <88953023+Montan-a@users.noreply.github.com>
2024-08-15 21:54:58 -0500

    implement thank you page

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,12 @@
+import ThankYou from "@/components/ThankYou";
+import { Suspense } from "react";
+
+const Page = () => {
+  return (
+    <Suspense>
+      <ThankYou />
+    </Suspense>
+  );
+};
+
+export default Page;
