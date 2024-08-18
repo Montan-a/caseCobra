46b5d71ce04dded33bb087f0492932854348779d
Montana Shaw <88953023+Montan-a@users.noreply.github.com>
2024-08-18 12:39:46 -0500

    removed unused imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
-import type { Metadata } from "next";
-import { Inter, Recursive } from "next/font/google";
+import { Recursive } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
